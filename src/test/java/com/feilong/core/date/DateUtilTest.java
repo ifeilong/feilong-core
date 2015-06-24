38d79b9055b7e754c1296dbca48a0fe0f7e98ff4
feilong <venusdrogon@163.com>
2015-06-24 15:43:19 +0800

    update test

--- src/test/java/com/feilong/core/date/DateUtilTest.java
+++ src/test/java/com/feilong/core/date/DateUtilTest.java
@@ -526,10 +526,17 @@ public class DateUtilTest extends BaseDateUtilTest{
      */
     @Test
     public final void getIntervalHour(){
-        Date startDate = DateUtil.string2Date("2013-01-01 00:00:00", DatePattern.COMMON_DATE_AND_TIME);
-        log.debug(DateUtil.getIntervalHour(startDate, NOW) + "");
-        log.debug(DateUtil.getIntervalHour(startDate, DateUtil.string2Date("2113-01-01 00:00:00", DatePattern.COMMON_DATE_AND_TIME)) + "");
-        log.debug(DateUtil.getIntervalHour(startDate, DateUtil.string2Date("3113-01-01 00:00:00", DatePattern.COMMON_DATE_AND_TIME)) + "");
+        //        Date startDate = DateUtil.string2Date("2013-01-01 00:00:00", DatePattern.COMMON_DATE_AND_TIME);
+        //        log.debug(DateUtil.getIntervalHour(startDate, NOW) + "");
+        //        log.debug(DateUtil.getIntervalHour(startDate, DateUtil.string2Date("2113-01-01 00:00:00", DatePattern.COMMON_DATE_AND_TIME)) + "");
+        //        log.debug(DateUtil.getIntervalHour(startDate, DateUtil.string2Date("3113-01-01 00:00:00", DatePattern.COMMON_DATE_AND_TIME)) + "");
+        log.debug(StringUtil.format(
+                        "%05d",
+                        DateUtil.getIntervalHour(
+                                        DateUtil.string2Date("2014-01-01 00:00:00", DatePattern.COMMON_DATE_AND_TIME),
+                                        DateUtil.string2Date("2014-02-01 00:00:00", DatePattern.COMMON_DATE_AND_TIME)))
+
+                        + "");
     }
 
     /**
