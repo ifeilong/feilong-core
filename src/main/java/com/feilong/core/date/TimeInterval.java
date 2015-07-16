027a654aad4169056da0d0fe8b1489b4c3c5f9a9
feilong <venusdrogon@163.com>
2015-07-17 01:02:54 +0800

    change TimeInterval Integer/Long type to primitive type

--- src/main/java/com/feilong/core/date/TimeInterval.java
+++ src/main/java/com/feilong/core/date/TimeInterval.java
@@ -31,28 +31,35 @@ package com.feilong.core.date;
  * @author feilong
  * @version 1.0 2012-5-18 下午2:57:14
  * @version 1.0.5 2014-5-4 14:23 change to interface
+ * @version 1.2.2 2015-7-17 01:02 change Integer/Long type to primitive type
  * @since 1.0.0
  */
 public final class TimeInterval{
+
+    //由于 annotation 的属性, 只能声明成  private类型
+    //Invalid type int for the annotation attribute ClientCache.value; 
+    //only primitive type, String, Class, annotation, enumeration are permitted or 1-dimensional arrays thereof
+
+    //声明成 int等 primitive类型,方便 annotation使用, 
 
     /**
      * 1秒 1s.
      * 
      * @since 1.2.2
      * */
-    public static final Integer SECONDS_PER_SECOND      = 1;
+    public static final int  SECONDS_PER_SECOND      = 1;
 
     /** 1分钟 60s. */
-    public static final Integer SECONDS_PER_MINUTE      = 60 * SECONDS_PER_SECOND;
+    public static final int  SECONDS_PER_MINUTE      = 60 * SECONDS_PER_SECOND;
 
     /** 1小时 60 * 60=3600. */
-    public static final Integer SECONDS_PER_HOUR        = SECONDS_PER_MINUTE * 60;
+    public static final int  SECONDS_PER_HOUR        = SECONDS_PER_MINUTE * 60;
 
     /** 1天 60 * 60 * 24=86400. */
-    public static final Integer SECONDS_PER_DAY         = SECONDS_PER_HOUR * 24;
+    public static final int  SECONDS_PER_DAY         = SECONDS_PER_HOUR * 24;
 
     /** 一个星期 60 * 60 * 24 * 7= 604 800. */
-    public static final Integer SECONDS_PER_WEEK        = SECONDS_PER_DAY * 7;
+    public static final int  SECONDS_PER_WEEK        = SECONDS_PER_DAY * 7;
 
     /**
      * 30天 一个月 60 * 60 * 24 * 30= 2592000.
@@ -60,17 +67,17 @@ public final class TimeInterval{
      * 估值,没有精确一个月28/29天 还是30 31天.
      * </p>
      */
-    public static final Integer SECONDS_PER_MONTH       = SECONDS_PER_DAY * 30;
+    public static final int  SECONDS_PER_MONTH       = SECONDS_PER_DAY * 30;
 
     /**
      * 365天 1年 60 * 60 * 24 * 365=31536000.
      * <p>
      * Integer.MAX_VALUE:2147483647<br>
      * Integer.MIN_VALUE-2147483648<br>
-     * 一年数据为 31536000,所以 Integer 最大为 68.096259734906 年
+     * 一年数据为 31536000,所以 int 最大为 68.096259734906 年
      * </p>
      */
-    public static final Integer SECONDS_PER_YEAR        = SECONDS_PER_DAY * 365;
+    public static final int  SECONDS_PER_YEAR        = SECONDS_PER_DAY * 365;
 
     //***********************************************************************************
 
@@ -80,7 +87,7 @@ public final class TimeInterval{
      * @since 1.2.2
      * @see com.feilong.core.date.TimeInterval#SECONDS_PER_SECOND
      */
-    public static final Integer MILLISECOND_PER_SECONDS = 1000;
+    public static final int  MILLISECOND_PER_SECONDS = 1000;
 
     /**
      * 每分钟的毫秒数,1分钟 60 000ms.
@@ -88,7 +95,7 @@ public final class TimeInterval{
      * @since 1.2.1
      * @see com.feilong.core.date.TimeInterval#SECONDS_PER_MINUTE
      */
-    public static final Integer MILLISECOND_PER_MINUTE  = SECONDS_PER_MINUTE * 1000;
+    public static final int  MILLISECOND_PER_MINUTE  = SECONDS_PER_MINUTE * 1000;
 
     /**
      * 每小时的毫秒数,1小时 3600 000ms.
@@ -96,7 +103,7 @@ public final class TimeInterval{
      * @since 1.2.1
      * @see com.feilong.core.date.TimeInterval#SECONDS_PER_HOUR
      */
-    public static final Integer MILLISECOND_PER_HOUR    = SECONDS_PER_HOUR * 1000;
+    public static final int  MILLISECOND_PER_HOUR    = SECONDS_PER_HOUR * 1000;
 
     /**
      * 每天的毫秒数,1天 86400 000ms.
@@ -104,7 +111,7 @@ public final class TimeInterval{
      * @since 1.2.1
      * @see com.feilong.core.date.TimeInterval#SECONDS_PER_DAY
      */
-    public static final Integer MILLISECOND_PER_DAY     = SECONDS_PER_DAY * 1000;
+    public static final int  MILLISECOND_PER_DAY     = SECONDS_PER_DAY * 1000;
 
     /**
      * 每星期的毫秒数,一个星期 604 800 000ms.
@@ -112,7 +119,7 @@ public final class TimeInterval{
      * @since 1.2.1
      * @see com.feilong.core.date.TimeInterval#SECONDS_PER_WEEK
      */
-    public static final Integer MILLISECOND_PER_WEEK    = SECONDS_PER_WEEK * 1000;
+    public static final int  MILLISECOND_PER_WEEK    = SECONDS_PER_WEEK * 1000;
 
     /**
      * 每月的毫秒数,2592000 000ms,<span style="color:red">大于 {@link Integer#MAX_VALUE}:2147483647</span>.
@@ -124,7 +131,7 @@ public final class TimeInterval{
      * @since 1.2.1
      * @see com.feilong.core.date.TimeInterval#SECONDS_PER_MONTH
      */
-    public static final Long    MILLISECOND_PER_MONTH   = 1000L * SECONDS_PER_MONTH;
+    public static final long MILLISECOND_PER_MONTH   = 1000L * SECONDS_PER_MONTH;
 
     /**
      * 每年的毫秒数, 31536000 000ms,<span style="color:red">大于 {@link Integer#MAX_VALUE}:2147483647</span>
@@ -133,7 +140,7 @@ public final class TimeInterval{
      * @since 1.2.1
      * @see com.feilong.core.date.TimeInterval#SECONDS_PER_YEAR
      */
-    public static final Long    MILLISECOND_PER_YEAR    = 1000L * SECONDS_PER_YEAR;
+    public static final long MILLISECOND_PER_YEAR    = 1000L * SECONDS_PER_YEAR;
 
     //****************************************************************************************
 
