a66a312037c4139d6f3cb63cb7bfd534672579cc
feilong <venusdrogon@163.com>
2015-07-25 02:59:33 +0800

    ObjectUtil add  `T findTypeValue(Object findValue,Class<T> findedClassType)` method
    move ObjectUtil `Boolean toBoolean(Object object)`  method to
    ConvertUtil
    move ObjectUtil `Integer toInteger(Object value)`  method to ConvertUtil
    move ObjectUtil `BigDecimal toBigDecimal(Object value)`  method to
    ConvertUtil
    move ObjectUtil `Long toLong(Object value)`  method to ConvertUtil
    move ObjectUtil `Double toDouble(Object value)`  method to ConvertUtil
    move ObjectUtil `Float toFloat(Object value)`  method to ConvertUtil
    move ObjectUtil `Short toShort(Object value)`  method to ConvertUtil
    move ObjectUtil `String toString(Object value)`  method to ConvertUtil
    move ObjectUtil `<T> T toT(Object value,Class<T> klass)`  method to
    ConvertUtil

--- src/main/java/com/feilong/core/lang/ObjectUtil.java
+++ src/main/java/com/feilong/core/lang/ObjectUtil.java
@@ -17,8 +17,6 @@ package com.feilong.core.lang;
 
 import java.io.ByteArrayOutputStream;
 import java.io.Serializable;
-import java.math.BigDecimal;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Enumeration;
 import java.util.Iterator;
@@ -26,7 +24,9 @@ import java.util.Map;
 import java.util.Set;
 
 import org.apache.commons.collections4.iterators.EnumerationIterator;
+import org.apache.commons.lang3.ClassUtils;
 
+import com.feilong.core.bean.PropertyUtil;
 import com.feilong.core.io.SerializableUtil;
 import com.feilong.core.util.ArrayUtil;
 import com.feilong.core.util.Validator;
@@ -45,6 +45,49 @@ public final class ObjectUtil{
         //AssertionError不是必须的. 但它可以避免不小心在类的内部调用构造器. 保证该类在任何情况下都不会被实例化.
         //see 《Effective Java》 2nd
         throw new AssertionError("No " + getClass().getName() + " instances for you!");
+    }
+
+    /**
+     * Find type value.
+     *
+     * @param <T>
+     *            the generic type
+     * @param findValue
+     *            the find value
+     * @param findedClassType
+     *            the finded class type
+     * @return the t
+     * @since 1.2.3
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T findTypeValue(Object findValue,Class<T> findedClassType){
+        if (ClassUtil.isInstance(findValue, findedClassType)){
+            return (T) findValue;
+        }else{
+            //一般自定义的command 里面 就是些 string int,list map等对象
+            //这些我们过滤掉,只取 类型是 findedClassType的
+            if (!ClassUtils.isPrimitiveOrWrapper(findedClassType)//
+                            && !ClassUtil.isInstance(findValue, CharSequence.class)//
+                            && !ClassUtil.isInstance(findValue, Collection.class) //
+                            && !ClassUtil.isInstance(findValue, Map.class) //
+            ){
+                Map<String, Object> describe = PropertyUtil.describe(findValue);
+
+                for (Map.Entry<String, Object> entry : describe.entrySet()){
+                    String key = entry.getKey();
+                    Object value = entry.getValue();
+
+                    if (null != value && !"class".equals(key)){
+                        //级联查询
+                        T t = findTypeValue(value, findedClassType);
+                        if (null != t){
+                            return t;
+                        }
+                    }
+                }
+            }
+        }
+        return null;
     }
 
     /**
@@ -167,7 +210,7 @@ public final class ObjectUtil{
      * @param nullTypeFlag
      *            标识null和""相比的情况,默认值为false 标识不相等
      * @return 判断两个值是否相等
-     * @see java.util.Objects#equals(Object, Object)
+     * @see "java.util.Objects#equals(Object, Object)"
      * @see org.apache.commons.lang3.ObjectUtils#equals(Object, Object)
      */
     public static final Boolean equals(Object object,Object object2,boolean nullTypeFlag){
@@ -211,7 +254,7 @@ public final class ObjectUtil{
      * @param object2
      *            object2
      * @return 判断两个值是否相等 标识null和""相比的情况,默认值为false 标识不相等
-     * @see java.util.Objects#equals(Object, Object)
+     * @see "java.util.Objects#equals(Object, Object)"
      * @see org.apache.commons.lang3.ObjectUtils#equals(Object, Object)
      */
     public static final Boolean equals(Object object,Object object2){
@@ -242,261 +285,6 @@ public final class ObjectUtil{
      */
     public static final Boolean isInteger(Object object){
         return object instanceof Integer;
-    }
-
-    /**
-     * object 类型转换成boolean类型.
-     * 
-     * @param object
-     *            object
-     * @return boolean
-     */
-    public static final Boolean toBoolean(Object object){
-        if (null == object){
-            throw new IllegalArgumentException("object can't be null/empty!");
-        }
-        return Boolean.parseBoolean(object.toString());
-    }
-
-    /**
-     * object转成integer类型.
-     *
-     * @param value
-     *            值
-     * @return <ul>
-     *         <li>如果value是null,则返回null</li>
-     *         <li>如果value本身是Integer类型,则强制转换成 (Integer) value</li>
-     *         <li>否则 new Integer(value.toString().trim())</li>
-     *         <li>如果value不能转成integer 会抛出 IllegalArgumentException异常</li>
-     *         </ul>
-     */
-    public static final Integer toInteger(Object value){
-        if (Validator.isNullOrEmpty(value)){
-            return null;
-        }
-
-        if (value instanceof Integer){
-            return (Integer) value;
-        }
-
-        try{
-            return new Integer(trim(value));
-        }catch (Exception e){
-            throw new IllegalArgumentException("Input param:[\"" + value + "\"], convert to integer exception", e);
-        }
-    }
-
-    /**
-     * object类型转换成BigDecimal.
-     * 
-     * <h3>注意:对于 double 转成 BigDecimal:</h3>
-     * 
-     * <blockquote>
-     * 
-     * <pre>
-     * <span style="color:red">推荐使用 BigDecimal.valueOf</span>，不建议使用new BigDecimal(double)，参见 JDK API
-     * new BigDecimal(0.1) ====&gt;   0.1000000000000000055511151231257827021181583404541015625
-     * BigDecimal.valueOf(0.1) ====&gt;  0.1
-     * </pre>
-     * 
-     * </blockquote>
-     * 
-     * @param value
-     *            值
-     * @return BigDecimal
-     */
-    public static final BigDecimal toBigDecimal(Object value){
-        if (Validator.isNullOrEmpty(value)){
-            return null;
-        }
-
-        if (value instanceof BigDecimal){
-            return (BigDecimal) value;
-        }
-
-        //对于 double 转成 BigDecimal，推荐使用 BigDecimal.valueOf，不建议使用new BigDecimal(double)，参见 JDK API
-        //new BigDecimal(0.1) ====>   0.1000000000000000055511151231257827021181583404541015625
-        //BigDecimal.valueOf(0.1) ====>  0.1
-
-        //先转成string 就可以了
-        return new BigDecimal(trim(value));
-    }
-
-    /**
-     * 把对象转换为long类型.
-     * 
-     * @param value
-     *            包含数字的对象.
-     * @return long 转换后的数值,对不能转换的对象返回null.
-     */
-    public static final Long toLong(Object value){
-        if (Validator.isNullOrEmpty(value)){
-            return null;
-        }
-
-        if (value instanceof Long){
-            return (Long) value;
-        }
-        return Long.parseLong(value.toString());
-    }
-
-    /**
-     * Object to double.
-     * 
-     * @param value
-     *            the value
-     * @return the double
-     */
-    public static final Double toDouble(Object value){
-        if (Validator.isNullOrEmpty(value)){
-            return null;
-        }
-
-        if (value instanceof Double){
-            return (Double) value;
-        }
-        return new Double(value.toString());
-    }
-
-    /**
-     * object to float.
-     * 
-     * @param value
-     *            the value
-     * @return the float
-     */
-    public static final Float toFloat(Object value){
-        if (Validator.isNullOrEmpty(value)){
-            return null;
-        }
-
-        if (value instanceof Float){
-            return (Float) value;
-        }
-        return new Float(value.toString());
-    }
-
-    /**
-     * object to short.
-     * 
-     * @param value
-     *            the value
-     * @return the short
-     */
-    public static final Short toShort(Object value){
-        if (Validator.isNullOrEmpty(value)){
-            return null;
-        }
-
-        if (value instanceof Short){
-            return (Short) value;
-        }
-        return new Short(value.toString());
-    }
-
-    /**
-     * 把对象转换成字符串.
-     * 
-     * @param value
-     *            参数值
-     * @return <ul>
-     *         <li>{@code (null == value) =====>return null}</li>
-     *         <li>{@code (value instanceof String) =====>return (String) value}</li>
-     *         <li>{@code return value.toString()}</li>
-     *         <li>对于数组，将会调用 {@link java.util.Arrays#toString(Object[])}</li>
-     *         </ul>
-     * @since 1.0
-     */
-    public static final String toString(Object value){
-        if (null == value){
-            return null;
-        }
-        if (value instanceof String){
-            return (String) value;
-        }
-        if (value instanceof Object[]){
-            return Arrays.toString((Object[]) value);
-        }
-
-        //***************************************************************
-        // primitive ints
-        if (value instanceof int[]){
-            return Arrays.toString((int[]) value);
-        }
-
-        // primitive long
-        if (value instanceof long[]){
-            return Arrays.toString((long[]) value);
-        }
-
-        // primitive float
-        if (value instanceof float[]){
-            return Arrays.toString((float[]) value);
-        }
-
-        // primitive double
-        if (value instanceof double[]){
-            return Arrays.toString((double[]) value);
-        }
-
-        // primitive char
-        if (value instanceof char[]){
-            return Arrays.toString((char[]) value);
-        }
-
-        // primitive boolean
-        if (value instanceof boolean[]){
-            return Arrays.toString((boolean[]) value);
-        }
-
-        // primitive byte
-        if (value instanceof byte[]){
-            return Arrays.toString((byte[]) value);
-        }
-
-        // primitive short
-        if (value instanceof short[]){
-            return Arrays.toString((short[]) value);
-        }
-        return value.toString();
-    }
-
-    /**
-     * object to T.
-     * 
-     * @param <T>
-     *            the generic type
-     * @param value
-     *            the value
-     * @param klass
-     *            the class1
-     * @return if null==value return null,else to class convert<br>
-     *         如果不是内置的class,将使用强制转换 (T) value
-     */
-    @SuppressWarnings("unchecked")
-    // XXX
-    public static final <T> T toT(Object value,Class<T> klass){
-        if (null == value){
-            return null;
-        }
-        if (klass == String.class){
-            return (T) toString(value);
-        }else if (klass == Boolean.class){
-            return (T) toBoolean(value);
-        }else if (klass == Integer.class){
-            return (T) toInteger(value);
-        }else if (klass == BigDecimal.class){
-            return (T) toBigDecimal(value);
-        }else if (klass == Long.class){
-            return (T) toLong(value);
-        }else if (klass == Double.class){
-            return (T) toDouble(value);
-        }else if (klass == Float.class){
-            return (T) toFloat(value);
-        }else if (klass == Short.class){
-            return (T) toShort(value);
-        }
-        return (T) value;
     }
 
     /**
