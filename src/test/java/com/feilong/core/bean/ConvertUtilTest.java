daa6893eb4c0739a7ea0ccea08d21dc820b36ca2
feilong <venusdrogon@163.com>
2015-08-06 01:44:54 +0800

    update test class

--- src/test/java/com/feilong/core/bean/ConvertUtilTest.java
+++ src/test/java/com/feilong/core/bean/ConvertUtilTest.java
@@ -28,12 +28,15 @@ import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.StringTokenizer;
 
+import org.apache.commons.beanutils.ConvertUtils;
 import org.apache.commons.beanutils.Converter;
 import org.apache.commons.beanutils.converters.ArrayConverter;
 import org.apache.commons.beanutils.converters.IntegerConverter;
+import org.apache.commons.beanutils.converters.LongConverter;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
@@ -369,5 +372,83 @@ public class ConvertUtilTest{
         while (enumeration.hasMoreElements()){
             LOGGER.info("" + enumeration.nextElement());
         }
+    }
+
+    /**
+     * Test Converting using the IntegerConverter as the component Converter.
+     */
+    @Test
+    public void testComponentIntegerConverter(){
+        String stringA = "1,111; 2,222; 3,333; 4,444";
+
+        Class<Long[]> defaultType = Long[].class;
+        Long[] result = toLongArray(defaultType, stringA);
+
+        System.out.println(JsonUtil.format(result));//TODO:remove
+    }
+
+    /**
+     * To long array.
+     *
+     * @param <T>
+     *            the generic type
+     * @param defaultType
+     *            the default type
+     * @param stringA
+     *            the string a
+     * @return the t
+     * @since 1.2.2
+     */
+    private <T> T toLongArray(Class<T> defaultType,String stringA){
+        LongConverter elementConverter = new LongConverter(new Long(0L));
+        elementConverter.setPattern("#,###");
+        elementConverter.setLocale(Locale.US);
+
+        return convert(defaultType, elementConverter, stringA);
+    }
+
+    /**
+     * Convert.
+     *
+     * @param <T>
+     *            the generic type
+     * @param defaultArrayType
+     *            默认的数组类型
+     * @param individualArrayElementConverter
+     *            单个元素的 {@link Converter}
+     * @param toBeConvertedValue
+     *            需要被转换的值
+     * @return the t
+     * @since 1.2.2
+     */
+    private <T> T convert(Class<T> defaultArrayType,Converter individualArrayElementConverter,Object toBeConvertedValue){
+        char[] allowedChars = new char[] { ',', '-' };
+        char delimiter = ';';
+        boolean onlyFirstToString = true;
+
+        int defaultSize = 0;
+
+        //**********************************************************
+
+        ArrayConverter arrayConverter = new ArrayConverter(defaultArrayType, individualArrayElementConverter, defaultSize);
+        arrayConverter.setAllowedChars(allowedChars);
+        arrayConverter.setDelimiter(delimiter);
+        arrayConverter.setOnlyFirstToString(onlyFirstToString);
+
+        T result = arrayConverter.convert(defaultArrayType, toBeConvertedValue);
+        return result;
+    }
+
+    /**
+     * TestConvertUtilsTest.
+     */
+    @Test
+    public void testConvertUtilsTest(){
+
+        System.out.println(ConvertUtils.convert(888.000f));
+
+        BigDecimal bigDecimal = (BigDecimal) ConvertUtils.convert(888.000f, BigDecimal.class);
+        System.out.println(bigDecimal);
+
     }
 }
