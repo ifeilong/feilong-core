342bb525186c070ed758bb2ffef580092b913b11
feilong <venusdrogon@163.com>
2015-09-04 05:14:44 +0800

    update javadoc

--- src/main/java/com/feilong/core/bean/ConvertUtil.java
+++ src/main/java/com/feilong/core/bean/ConvertUtil.java
@@ -360,7 +360,7 @@ public final class ConvertUtil{
 
     /**
      * 将集合使用连接符号链接成字符串.
-     *
+     * 
      * @param toStringConfig
      *            连接字符串 实体
      * @param collection
@@ -385,7 +385,7 @@ public final class ConvertUtil{
      * 将数组 通过 {@link ToStringConfig} 拼接成 字符串.
      * 
      * <p style="color:green">
-     * 支持包装类型以及原始类型,比如 Integer []arrays 以及 int []arrays
+     * 支持包装类型以及原始类型,比如 Integer [] arrays 以及 int []arrays
      * </p>
      * 
      * <code>
@@ -423,30 +423,26 @@ public final class ConvertUtil{
         }
 
         //************************************************************************
-        ToStringConfig useToStringConfig = null == toStringConfig ? new ToStringConfig() : toStringConfig;
         Object[] operateArray = toObjects(arrays);
+
+        ToStringConfig useToStringConfig = null == toStringConfig ? new ToStringConfig() : toStringConfig;
         String connector = useToStringConfig.getConnector();
         //************************************************************************
-
         StringBuilder sb = new StringBuilder();
-        for (int i = 0, j = operateArray.length; i < j; ++i){
-            @SuppressWarnings("unchecked")
-            T t = (T) operateArray[i];
 
-            //如果是null 或者 empty,但是参数值是不拼接,那么跳过,继续循环
-            if (Validator.isNullOrEmpty(t) && !useToStringConfig.getIsJoinNullOrEmpty()){
+        for (Object obj : operateArray){
+            //如果是null或者empty,但是参数值是不拼接,那么跳过,继续循环
+            if (Validator.isNullOrEmpty(obj) && !useToStringConfig.getIsJoinNullOrEmpty()){
                 continue;
             }
-            //value转换, 注意:如果 value 是null ,StringBuilder将拼接 "null" 字符串, 详见  java.lang.AbstractStringBuilder#append(String)
-            sb.append(null == t ? StringUtils.EMPTY : "" + t); //see StringUtils.defaultString(t)
+
+            //value转换,注意:如果 value是null,StringBuilder将拼接 "null" 字符串,详见  java.lang.AbstractStringBuilder#append(String)
+            sb.append(null == obj ? StringUtils.EMPTY : "" + obj); //see StringUtils.defaultString(t)
 
             if (null != connector){//注意可能传过来的是换行符 不能使用Validator.isNullOrEmpty来判断
-
-                //放心大胆的拼接 connector, 不判断是否是最后一个,最后会截取
-                sb.append(connector);
+                sb.append(connector);//放心大胆的拼接 connector, 不判断是否是最后一个,最后会截取
             }
         }
-
         return StringUtil.substringWithoutLast(sb, connector);
     }
 
