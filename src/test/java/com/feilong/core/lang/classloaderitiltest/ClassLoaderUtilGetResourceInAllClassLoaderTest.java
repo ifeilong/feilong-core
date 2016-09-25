55366d5e0930e61e7cb809ddcc82eaac7929cc09
feilong <venusdrogon@163.com>
2016-09-25 21:45:16 +0800

    add ClassLoaderUtilGetResourceInAllClassLoaderTest fix #475

---
+++ src/test/java/com/feilong/core/lang/classloaderitiltest/ClassLoaderUtilGetResourceInAllClassLoaderTest.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2008 feilong
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.feilong.core.lang.classloaderitiltest;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.feilong.core.lang.ClassLoaderUtil;
+
+/**
+ * The Class ClassLoaderUtilGetResourceInAllClassLoaderTest.
+ *
+ * @author <a href="http://feitianbenyue.iteye.com/">feilong</a>
+ */
+public class ClassLoaderUtilGetResourceInAllClassLoaderTest{
+
+    /**
+     * Test get resource not found.
+     */
+    @Test
+    public void testGetResourceNotFound(){
+        assertEquals(null, ClassLoaderUtil.getResourceInAllClassLoader("slf4j-log4j12-1.7.21", this.getClass()));
+        assertEquals(null, ClassLoaderUtil.getResourceInAllClassLoader("slf4j-log4j12-1.7.21.jar", this.getClass()));
+    }
+
+    /**
+     * Test get resource null resource name.
+     */
+    @Test(expected = NullPointerException.class)
+    public void testGetResourceNullResourceName(){
+        ClassLoaderUtil.getResourceInAllClassLoader(null, this.getClass());
+    }
+
+    //*************************************************************************************
+
+    /**
+     * Test get resource null calling class.
+     */
+    @Test
+    public void testGetResourceNullCallingClass(){
+        String path = "file:/E:/Workspaces/feilong/feilong-core/target/classes/com/feilong/core/lang/ArrayUtil.class";
+        String resourceName = "com/feilong/core/lang/ArrayUtil.class";
+        assertEquals(path, ClassLoaderUtil.getResourceInAllClassLoader(resourceName, null).toString());
+    }
+
+    /**
+     * Test get resource 232.
+     */
+    @Test
+    public void testGetResource232(){
+        String path = "file:/E:/Workspaces/feilong/feilong-core/target/classes/com/feilong/core/lang/ArrayUtil.class";
+        String resourceName = "com/feilong/core/lang/ArrayUtil.class";
+        assertEquals(path, ClassLoaderUtil.getResourceInAllClassLoader(resourceName, this.getClass()).toString());
+    }
+
+    /**
+     * Test get resource slan.
+     */
+    @Test
+    public void testGetResourceSlan(){
+        String path = "file:/E:/Workspaces/feilong/feilong-core/target/classes/com/feilong/core/lang/ArrayUtil.class";
+        String resourceName = "com/feilong/core/lang/ArrayUtil.class";
+        assertEquals(path, ClassLoaderUtil.getResourceInAllClassLoader("/" + resourceName, this.getClass()).toString());
+    }
+}
