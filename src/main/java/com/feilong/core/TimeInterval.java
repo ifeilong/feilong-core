726b6bf847fd4319191e9fda884f91f48b19fe8a
feilong <venusdrogon@163.com>
2016-10-10 15:56:50 +0800

    update javadoc

--- src/main/java/com/feilong/core/TimeInterval.java
+++ src/main/java/com/feilong/core/TimeInterval.java
@@ -18,16 +18,6 @@ package com.feilong.core;
 /**
  * 常用时间间隔.
  * 
- * <h3>注意:</h3>
- * 
- * <blockquote>
- * <p>
- * {@link Integer#MAX_VALUE}:2147483647<br>
- * {@link Integer#MIN_VALUE}:-2147483648<br>
- * 一年数据为 {@link #SECONDS_PER_YEAR} 31536000,所以 {@link Integer#MAX_VALUE} 为 68.096259734906 年
- * </p>
- * </blockquote>
- *
  * @author <a href="http://feitianbenyue.iteye.com/">feilong</a>
  * @see org.apache.commons.lang3.time.DateUtils
  * @since 1.0.0
@@ -37,6 +27,7 @@ public final class TimeInterval{
     //由于 annotation 的属性,只能声明成  primitive 类型
     //否则提示 :Invalid type int for the annotation attribute xxx;only "primitive type, String, Class, annotation, enumeration" are permitted or 1-dimensional arrays thereof
 
+    //------------------------------以秒为单位------------------------------
     /**
      * 1秒 1s.
      * 
@@ -53,31 +44,41 @@ public final class TimeInterval{
     /** 1天 60 * 60 * 24=86400. */
     public static final int  SECONDS_PER_DAY         = SECONDS_PER_HOUR * 24;
 
-    /** 一个星期 60 * 60 * 24 * 7= 604 800. */
+    /** 1个星期 60 * 60 * 24 * 7= 604 800. */
     public static final int  SECONDS_PER_WEEK        = SECONDS_PER_DAY * 7;
 
     /**
-     * 30天 一个月 60 * 60 * 24 * 30= 2592000.
-     * <p>
-     * 估值,没有精确一个月28/29天 还是30 31天.
-     * </p>
+     * 1个月 60 * 60 * 24 * 30= 2592000.
+     * 
+     * <h3>说明:</h3>
+     * <blockquote>
+     * <ol>
+     * <li>30天一个月 ,<b>估值</b>,没有精确一个月28/29天 还是30 31天.</li>
+     * </ol>
+     * </blockquote>
      */
     public static final int  SECONDS_PER_MONTH       = SECONDS_PER_DAY * 30;
 
     /**
-     * 365天 1年 60 * 60 * 24 * 365=31536000.
-     * <p>
-     * Integer.MAX_VALUE:2147483647<br>
-     * Integer.MIN_VALUE-2147483648<br>
-     * 一年数据为 31536000,所以 int 最大为 68.096259734906 年
-     * </p>
+     * 1年 60 * 60 * 24 * 365=31536000.
+     * 
+     * <h3>说明:</h3>
+     * <blockquote>
+     * <ol>
+     * <li>以365天算.</li>
+     * <li>
+     * {@link Integer#MAX_VALUE}:2147483647, {@link Integer#MIN_VALUE}:-2147483648<br>
+     * 1年数据为 {@link #SECONDS_PER_YEAR} 31536000,所以 {@link Integer#MAX_VALUE} 为 68.096259734906 年,注意使用的时候,超过最大值使用long来计算
+     * </li>
+     * </ol>
+     * </blockquote>
      */
     public static final int  SECONDS_PER_YEAR        = SECONDS_PER_DAY * 365;
 
-    //***********************************************************************************
+    //------------------------------以毫秒为单位------------------------------
 
     /**
-     * 每秒的毫秒数,1秒 1000ms.
+     * 1秒 1000ms(毫秒).
      *
      * @see #SECONDS_PER_SECOND
      * @see org.apache.commons.lang3.time.DateUtils#MILLIS_PER_SECOND
@@ -86,7 +87,7 @@ public final class TimeInterval{
     public static final int  MILLISECOND_PER_SECONDS = 1000;
 
     /**
-     * 每分钟的毫秒数,1分钟 60 000ms.
+     * 1分钟 60 000ms(毫秒).
      *
      * @see #SECONDS_PER_MINUTE
      * @see org.apache.commons.lang3.time.DateUtils#MILLIS_PER_MINUTE
@@ -95,7 +96,7 @@ public final class TimeInterval{
     public static final int  MILLISECOND_PER_MINUTE  = SECONDS_PER_MINUTE * 1000;
 
     /**
-     * 每小时的毫秒数,1小时 3600 000ms.
+     * 1小时 3600 000ms(毫秒).
      *
      * @see #SECONDS_PER_HOUR
      * @see org.apache.commons.lang3.time.DateUtils#MILLIS_PER_HOUR
@@ -104,7 +105,7 @@ public final class TimeInterval{
     public static final int  MILLISECOND_PER_HOUR    = SECONDS_PER_HOUR * 1000;
 
     /**
-     * 每天的毫秒数,1天 86400 000ms.
+     * 1天 86400 000ms(毫秒).
      *
      * @see #SECONDS_PER_DAY
      * @see org.apache.commons.lang3.time.DateUtils#MILLIS_PER_DAY
@@ -113,7 +114,7 @@ public final class TimeInterval{
     public static final int  MILLISECOND_PER_DAY     = SECONDS_PER_DAY * 1000;
 
     /**
-     * 每星期的毫秒数,一个星期 604 800 000ms.
+     * 1个星期 604 800 000ms(毫秒).
      *
      * @see #SECONDS_PER_WEEK
      * @since 1.2.1
@@ -121,19 +122,29 @@ public final class TimeInterval{
     public static final int  MILLISECOND_PER_WEEK    = SECONDS_PER_WEEK * 1000;
 
     /**
-     * 每月的毫秒数,2592000 000ms,<span style="color:red">大于 {@link Integer#MAX_VALUE}:2147483647</span>.
+     * 1个月,2592000 000ms(毫秒),<span style="color:red">大于 {@link Integer#MAX_VALUE}:2147483647</span>.
      * 
-     * <p>
-     * 30天一个月 ,估值,没有精确一个月28/29天 还是30 31天.
-     * </p>
-     *
+     * <h3>说明:</h3>
+     * <blockquote>
+     * <ol>
+     * <li>30天一个月 ,<b>估值</b>,没有精确一个月28/29天 还是30 31天.</li>
+     * </ol>
+     * </blockquote>
+     * 
      * @see #SECONDS_PER_MONTH
      * @since 1.2.1
      */
     public static final long MILLISECOND_PER_MONTH   = 1000L * SECONDS_PER_MONTH;
 
     /**
-     * 每年的毫秒数, 31536000 000ms,<span style="color:red">大于 {@link Integer#MAX_VALUE}:2147483647</span>.
+     * 1年 31536000 000ms(毫秒).
+     * 
+     * <h3>说明:</h3>
+     * <blockquote>
+     * <ol>
+     * <li>以365天算.</li>
+     * </ol>
+     * </blockquote>
      *
      * @see #SECONDS_PER_YEAR
      * @since 1.2.1
