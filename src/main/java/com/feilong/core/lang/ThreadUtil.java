aa18c395901cb998958e5e09470d799aabcb1218
feilong <venusdrogon@163.com>
2020-03-31 20:22:16 +0800

    :pencil: update javadoc

--- src/main/java/com/feilong/core/lang/ThreadUtil.java
+++ src/main/java/com/feilong/core/lang/ThreadUtil.java
@@ -86,6 +86,7 @@ import com.feilong.core.lang.thread.PartitionThreadConfig;
  * @see "org.springframework.core.task.SimpleAsyncTaskExecutor"
  * @see "org.springframework.core.task.SyncTaskExecutor"
  * @see "org.springframework.core.task.TaskExecutor"
+ * @see java.util.concurrent.ThreadFactory
  * @since 1.10.3
  */
 public final class ThreadUtil{
